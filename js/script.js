8066258b61680577a789439afc6e3143f9570730
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-03-11 17:17:42 +0100

    Make a function that generates a grid with any size

--- js/script.js
+++ js/script.js
@@ -8,22 +8,37 @@ function getRandomColor() {
 }
 
 
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const newRow = document.createElement("div");
+        newRow.classList.add("grid-row");
+        container.appendChild(newRow);
+    
+        for (let j = 0; j < size; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("square");
+            newRow.appendChild(newDiv);
+        }
+    }
+
+    document.querySelectorAll(".grid-row").forEach((square) => {
+        square.style.width = `calc(100% / ${size})`;
+    });
+
+    document.querySelectorAll(".square").forEach((square) => {
+        square.style.height = `calc(100% / ${size})`;
+    });
+}
+
+
 const container = document.querySelector(".container")
 
-for (let i = 0; i < 16; i++) {
-    const newRow = document.createElement("div");
-    newRow.classList.add("grid-row")
-    container.appendChild(newRow);
-
-    for (let j = 0; j < 16; j++) {
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("square")
-        newRow.appendChild(newDiv);
-    }
-}
 
 container.addEventListener("mouseover", (event) => {
     if (event.target.classList.contains("square")) {
         event.target.style.background = getRandomColor();
     }
 });
+
+
+createGrid(10);
