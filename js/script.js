809419cf30efc6b41c1cb2bd59b51431a7e578e7
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-03-11 16:08:59 +0100

    Create a function that generates a random color

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
 const container = document.querySelector(".container")
 
 for (let i = 0; i < 16; i++) {
@@ -11,3 +21,4 @@ for (let i = 0; i < 16; i++) {
         newRow.appendChild(newDiv);
     }
 }
+
