d448e13af5b32b18b7c7b062aadec2f3e36edb6b
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-03-11 15:54:04 +0100

    Create a grid 16x16

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 const container = document.querySelector("#container")
 
 for (let i = 0; i < 16; i++) {
+    const newRow = document.createElement("div");
+    newRow.classList.add("grid-row")
+    container.appendChild(newRow);
+
     for (let j = 0; j < 16; j++) {
         const newDiv = document.createElement("div");
-        
+        newDiv.classList.add("square")
+        newRow.appendChild(newDiv);
     }
 }
